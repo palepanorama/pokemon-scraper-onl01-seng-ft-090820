685f4c4a9426330dbe18704ed0fe18c9a9c501df
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-10-09 14:45:07 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,10 @@
 class Pokemon
+  attr_accessor :name, :id, :type, :db 
+  
+  def initialize(id = nil, name, type, db)
+    @id = id
+    @name = name 
+    @type = type 
+    @db = db 
+  end 
 end

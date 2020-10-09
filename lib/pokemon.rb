c4e3f9e2a372ca72a4570ed64ef6a50a6b309c53
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-10-09 14:48:35 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,7 +5,7 @@ class Pokemon
   end 
   
   def self.save(name, type, db)
-    sql = "INSERT INTO pokemon(name, type) VALUES (?, ?)", name, type 
+    sql = "INSERT INTO pokemon(name, type) VALUES (?, ?), name, type 
     db.execute(sql)
   end 
 end
